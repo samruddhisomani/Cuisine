80226dfd42377c77df14ade08f36f5c14f59e94b
Samruddhi Somani <samruddhi.somani@gmail.com>
2015-11-24 22:35:38 -0600

    fixing up svr

--- Code/svr.py
+++ Code/svr.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 execfile('tfidf.py')
 
+from sklearn.linear_model import SGDClassifier
+from sklearn.cross_validation import train_test_split
+import sam
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, y_train, y_test = train_test_split(x, cuisine, test_size=0.50, random_state=42)
+s=SGDClassifier(n_iter=20,random_state=0)
+s.fit(X_train,y_train)
+answer=s.predict(X_test)
+accuracy_score(y_test,answer)
