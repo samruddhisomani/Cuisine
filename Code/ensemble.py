958ee8f747c998e072e4aa6dce256b18cd6adac9
lchen3170 <lchen3170@gmail.com>
2015-12-02 12:53:42 -0600

    lotsa stuff, rf heatmap

--- Code/ensemble.py
+++ Code/ensemble.py
@@ -45,8 +45,66 @@ def winner(x):
         elif p1<p2:
             return x[1]
    
+def loser(x):
+    
+    if pd.isnull(x[1]):
+        return x[0]
+     
+    elif pd.notnull(x[1]) and pd.isnull(x[2]):
+        p1=priors.loc[x[0]]
+        p2=priors.loc[x[1]]
+
+        if p1<p2:
+            return x[0]
+        elif p2<p1:
+            return x[1]
+        
+    else:        
+        p1=priors.loc[x[0]]
+        p2=priors.loc[x[1]]
+        p3=priors.loc[x[2]]
+
+        if p1<p2 and p1<p3:
+            return x[0]
+        elif p2<p1 and p2<p3:
+            return x[1]
+        elif p3<p1 and p3<p2:
+            return x[2]
+            
+def winner2(x):
+    
+    if pd.isnull(x[1]):
+        return x[0]
+     
+    elif pd.notnull(x[1]) and pd.isnull(x[2]):
+        p1=priors.loc[x[0]]
+        p2=priors.loc[x[1]]
+
+        if p1<p2:
+            return x[1]
+        elif p2<p1:
+            return x[0]
+        
+    else:        
+        p1=priors.loc[x[0]]
+        p2=priors.loc[x[1]]
+        p3=priors.loc[x[2]]
+
+        if p1>p2 and p1>p3:
+            return x[0]
+        elif p2>p1 and p2>p3:
+            return x[1]
+        elif p3>p1 and p3>p2:
+            return x[2]
+            
 answers=predictions.apply(winner,axis=1)
+<<<<<<< Updated upstream
 
 SubFile(test['id'].reset_index(),pd.Series(answers),'../Submissions/ensemble.csv')
 
 pd.concat([test['id'],answers],axis=1)
+=======
+answer2=predictions.apply(loser,axis=1)
+answer3=predictions.apply(winner2,axis=1)
+#answer3.to_csv('../Submissions/ensemble_winner2.csv',index_label='id',header=['cuisine'])
+>>>>>>> Stashed changes
--- Code/randomforest.py
+++ Code/randomforest.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.grid_search import GridSearchCV
+import sam
 
 execfile('tfidf.py')
 
@@ -18,7 +19,7 @@ c.best_estimator_    #depth = 8, trees = 1000
 c.best_estimator_.oob_score_      #0.42744
 
 
-rf2=RandomForestClassifier(max_depth = 25, n_estimators = 1000,warm_start=True, random_state=0,n_jobs=-1,oob_score=True)
+rf2=RandomForestClassifier(max_depth = 25, n_estimators = 1000,warm_start=True, random_state=0,n_jobs=-1)
 rf2.fit(X,cuisine)
 
 rf_test = v.transform(test["ingredients"])
@@ -34,3 +35,14 @@ param_grid={'max_depth':[8,15,25],'n_estimators':[100,500,1000]}
 c.fit(x,cuisine)
 c.best_estimator_   #depth = 25, trees = 1000      ~55% accuracy
 c.best_score_        #0.5555
+
+
+
+#heatmap
+rf2.fit(X_train,y_train)
+rf_hm_pred = rf2.predict(x)
+rf_hm_df = ConfusionMatrix(cuisine,rf_hm_pred,"precision")
+rf_cm = confusion_matrix(y_test.as_matrix(),rf_hm_pred,labels=rf_hm_df.index)
+heatmap(rf_cm,xticklabels=rf_hm_df.index,yticklabels=rf_hm_df.index)
+heatmap(rf_hm_df)
+hmwrapper(rf_hm_df,'../Images/rf_heatmap.png')
