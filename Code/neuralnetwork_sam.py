143c32926ce0909d7f7f9df7009a1885ced652ce
samruddhisomani <sammysamhy@gmail.com>
2015-12-07 14:14:03 -0600

    neural net cv

--- Code/neuralnetwork_sam.py
+++ Code/neuralnetwork_sam.py
@@ -7,6 +7,7 @@ Created on Sat Dec 05 11:22:40 2015
 
 from sknn.mlp import Classifier,Layer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.grid_search import GridSearchCV
 execfile('Original.py')
 
 L=LabelEncoder()
@@ -15,8 +16,11 @@ y_=L.fit_transform(cuisine)
 nn=Classifier(random_state=0,
               layers=[Layer('Maxout',units=100,pieces=2),
                       Layer('Softmax')],
-            n_iter=25,
-            batch_size=100)
-nn.fit(x,y_)
+              batch_size=100)
 
-nn.score(x,y_)
+params={'learning_rate': [.01,.1]
+        'n_iter':[10,25,50]}
+        
+cv=GridSearchCV(nn,params,cv=2,n_jobs=2)
+
+cv.fit(x,cuisine)
